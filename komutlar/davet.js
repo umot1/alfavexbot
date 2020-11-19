92bc00bd94c9f033ac93a2a9ea627edf2c203ee7
Glitch (alfa-altyap1) <none>
2020-11-19 17:26:01 +0000

    🐧🌝 Checkpoint ./komutlar/davet.js:8706422/1078

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -6,9 +6,9 @@ message.channel.send(new Discord.MessageEmbed()
 .setThumbnail(message.author.avatarURL() ? message.author.avatarURL({dynamic: true}) : 'https://cdn.glitch.com/8e70d198-9ddc-40aa-b0c6-ccb4573f14a4%2F6499d2f1c46b106eed1e25892568aa55.png')
 .setDescription(`>>> ${client.user} kullanırken lütfen \`@${client.user.username}\` rolünü en yukarıda tutunuz!
 
-**Rollü Davet İçin:** [Buraya tıkla](https://discord.com/oauth2/authorize?client_id=777220232736931880&scope=bot&permissions=8)
+**Rollü Davet İçin:** [Buraya tıkla](https://discord.com/oauth2/authorize?client_id=777285108285177886&scope=bot&permissions=8)
 
-**Rolsüz Davet İçin:** [Buraya tıkla](https://discord.com/oauth2/authorize?client_id=777220232736931880&scope=bot&permissions=0)
+**Rolsüz Davet İçin:** [Buraya tıkla](https://discord.com/oauth2/authorize?client_id=777285108285177886&scope=bot&permissions=0)
 
 **Rollü Davet İçin:** [Buraya tıkla](https://discord.gg/kYSJneDpNf)
 \`\`\`https://discord.gg/kYSJneDpNf\`\`\``));
