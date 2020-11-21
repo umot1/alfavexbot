85e1e948c9ec900138c391728b7b46cf908d9213
Glitch (alfa-altyap1) <none>
2020-11-21 13:18:35 +0000

    🎪💇 Checkpoint ./komutlar/toplu-rol-sistem.js:25203527/27 ./komutlar/bansay.js:25203527/521 ./komutlar/duyuru.js:25203527/12238 ./komutlar/kick.js:25203527/1884 ./komutlar/jail.js:25203527/12882 ./komutlar/yilbasi.js:25203527/504 ./package.json:25203527/1371

--- komutlar/duyuru.js
+++ komutlar/duyuru.js
@@ -1,20 +1,21 @@
 const Discord = require('discord.js');
-const data = require('quick.db');
 
-exports.run = async (client, message, args) => {
-if(!message.member.permissions.has('ADMINISTRATOR')) return message.channel.send(new Discord.MessageEmbed().setThumbnail(message.author.avatarURL() ? message.author.avatarURL({dynamic: true}) : 'https://cdn.glitch.com/8e70d198-9ddc-40aa-b0c6-ccb4573f14a4%2F6499d2f1c46b106eed1e25892568aa55.png').setImage('https://cdn.glitch.com/0c8ef551-5187-48a8-9daf-f2cc35630f21%2Fyoneticigif.gif').setTitle('Bir hata oldu!').setDescription(`• \`${client.ayarlar.prefix}duyuru | ${client.ayarlar.prefix}yaz\` **kullanmak için,** \`Yönetici\` **yetkisine sahip olman gerekiyor.**`));
-if(!args[0]) return message.channel.send(new Discord.MessageEmbed().setTitle('Bir hata oldu!').setDescription(`${message.author} İçerik için yazı yazmayı unuttunuz.`));
-message.delete();
-return message.channel.send(new Discord.MessageEmbed().setDescription(args[0]));
+exports.run = (client, message, args) => {
+  let mesaj = args.slice(0).join(' ');
+if (mesaj.length < 1) return message.reply('Yazmam için herhangi bir şey yazmalısın.');
+  message.delete();
+  message.channel.send(mesaj);//wenzy code
+};//aga embedli yapmayı bilmiorm
 
-};
 exports.conf = {
   enabled: true,
-  guildOnly: true,
-  aliases: ['yaz'],
+  guildOnly: false,
+  aliases: ['say', 'söyle'],
   permLevel: 0
-}
+};
 
 exports.help = {
-  name: 'duyuru'
+  name: 'yaz',
+  description: 'İstediğiniz şeyi bota yazdırır.',
+  usage: 'yaz [yazdırmak istediğiniz şey]'
 };
